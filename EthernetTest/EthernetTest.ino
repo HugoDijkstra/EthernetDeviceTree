92c8a9ab7aa5c5ed528134b8cb59d22e4c0fc834
Hugo Dijkstra <hugodijkstra@live.nl>
2019-06-25 20:08:12 +0200

    Added arguments

--- EthernetTest/EthernetTest.ino
+++ EthernetTest/EthernetTest.ino
@@ -1,31 +1,25 @@
 #include "networkdevice.h"
 
-void TestCommand()
+void TestCommand(String args)
 {
-  Serial.println("TEST TEST: ");
+  Serial.println("TEST TEST: " + args);
+}
+
+void SetLed(String args)
+{
+
 }
 
 NetworkDevice device;
 String a = "TestCommand";
-String b = "int,string";
+String b = "2";
 void setup() {
   Serial.begin(9600);
   device.RegisterCommand(a, &TestCommand, b);
-  bool done = device.Init("AABBCCDD11223344", "Testing", "A Testing Arduino");
-  while (!done)
-  {
-    done = device.Init("AABBCCDD11223344", "Testing", "A Testing Arduino");
-  }
+  device.RegisterCommand("SetLed", &SetLed, "1");
+  device.setup("AABBCCDD11223344", "HomeArduino", "A Testing Arduino");
 }
 
 void loop() {
-  if (!client.connected()) {
-    bool done = device.Init("AABBCCDD11223344", "Testing", "A Testing Arduino");
-    while (!done)
-    {
-      done = device.Init("AABBCCDD11223344", "Testing", "A Testing Arduino");
-    }
-  }
-  device.HandleCommand();
-
+  device.loop("AABBCCDD11223344", "HomeArduino", "A Testing Arduino");
 }
--- EthernetTest/networkdevice.h
+++ EthernetTest/networkdevice.h
@@ -1,7 +1,11 @@
+// TODO: make easy inhert class for use
+// TODO: document easy inhert class (extra: write pdf documentation?)
+
+
 #ifndef NETWORKDEVICE
 #define NETWORKDEVICE
 
-#include <Ethernet.h>
+#include <Ethernet2.h>
 #include <SPI.h>
 
 #include <HashMap.h>
@@ -9,10 +13,14 @@
 EthernetClient client;
 
 class Command;
+class ValueRegister;
+
 const byte HASH_SIZE = 100;
 HashType<String*, Command*>FunctionCommands[HASH_SIZE];
 HashMap<String*, Command*> hashMap = HashMap<String*, Command*>(FunctionCommands, HASH_SIZE);
 
+HashType<String*, ValueRegister*>valueRegisters[HASH_SIZE];
+HashMap<String*, ValueRegister*> hashMap = HashMap<String*, ValueRegister*>(valueRegister, HASH_SIZE);
 class Command
 {
   public:
@@ -25,26 +33,38 @@ class Command
     String types;
 };
 
+template<typename T>
+class ValueRegister
+{
+  public:
+    T* object;
+}
+
 class NetworkDevice
 {
   public:
-    byte mac[6] = {0xDD, 0xFF, 0xBB, 0xEF, 0xFE, 0xED};
+    byte mac[6] = { 0xA8, 0x61, 0x0A, 0xAE, 0x01, 0x45 };
     byte server[4] = { 192, 168, 1, 3 };
-    byte ip[4] = { 192, 168, 1, 4 };
+    byte ip[4] = { 192, 168, 1, 13 };
+    String authKey;
+    String name;
+    String description;
     bool Init(String authKey, String name, String description)
     {
+      pinMode(6, OUTPUT);
+      digitalWrite(6, LOW);
       // put your setup code here, to run once:
-      Serial.println("Initialize Ethernet with DHCP:");
+      Serial.println("Initialize Ethernet:");
       Ethernet.begin(mac, ip);
-      //if (Ethernet.begin(mac, ip) == 0) {
+      //if (Ethernet.begin(mac) == 0) { // TODO fixed ethernet2.h suport!
       //  Serial.println("Failed to configure Ethernet using DHCP");
-      //  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-      //    Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
-      //  } else if (Ethernet.linkStatus() == LinkOFF) {
-      //    Serial.println("Ethernet cable is not connected.");
-      //  }
-      //  return false;
+      //if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+      //  Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+      //} else
+      //if (Ethernet.linkStatus() == LinkOFF) {
+      //  Serial.println("Ethernet cable is not connected.");
       //}
+      // }
       Serial.print("My IP address: ");
       Serial.println(Ethernet.localIP());
       Serial.println("Starting Connection");
@@ -62,12 +82,35 @@ class NetworkDevice
         {
           SendRegisterCommand((*hashMap[i].getHash()), hashMap[i].getValue());
         }
+        SendMessageToServer("COMPLETE");
       }
       else
       {
         return false;
       }
       return true;
+    }
+
+    void setup(String authKey, String name, String description)
+    {
+      bool done = Init(authKey, name, description);
+      while (!done)
+      {
+        done = Init(authKey, name, description);
+      }
+    }
+
+    void loop(String authKey, String name, String description)
+    {
+      if (!client.connected()) {
+        bool done = Init(authKey, name, description);
+        while (!done)
+        {
+          done = Init(authKey, name, description);
+        }
+      }
+      HandleCommand();
+
     }
 
     //INT=0,FLOAT=1,BOOL=2,STRING=3
@@ -88,6 +131,12 @@ class NetworkDevice
       }
     }
 
+    template<typename T>
+    void RegisterRequest(String name, T* type, string typeN)
+    {
+
+    }
+
     void HandleCommand()
     {
       if (client.connected())
@@ -96,38 +145,35 @@ class NetworkDevice
         int cs = client.available();
         if (cs == 0)
           return;
-        Serial.println(cs);
         while (cs)
         {
           char c = (char)client.read();
           command += c;
           cs--;
-          Serial.print(c);
         }
         String subCommand = command.substring(0, 3);
-        if (subCommand == "REG")
+        if (subCommand == "COM")
         {
-
-        }
-        else if (subCommand == "COM")
-        {
-
           String CommandString = command.substring(4);
+          String functionName = CommandString.substring(0, CommandString.indexOf(';'));
+          String arguments = CommandString.substring(CommandString.indexOf(';') + 1);
+          Serial.println(functionName);
           for (int i = 0; i < commandAmount; i++)
           {
-            if ((*hashMap[i].getHash()) == "TestCommand")
-            {
-              Serial.println("GOT IT");
-              void (*f)() = hashMap[i].getValue()->pointer;
-              f();
-              Serial.println("HOOO");
-              return;
-            }
-            Serial.println("\nNot");
+            Serial.println((*hashMap[i].getHash()));
+            if (functionName != (*hashMap[i].getHash()))
+              continue;
+            void(*f)(String f) = hashMap[i].getValue()->pointer;
+            f(arguments);
+            return;
           }
         }
-        else {
-          Serial.println("NO FITTING COMMAND");
+        else if (subCommand == "REQ") {
+
+        }
+        else if (subCommand == "INI")
+        {
+          Serial.Print
         }
       }
     }
